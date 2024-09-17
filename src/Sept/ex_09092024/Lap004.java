fd231335f469908909b01536103abf6153b62299
Chaitra Krishnaprasad <chaitra.kmsp@gmail.com>
2024-09-17 17:50:46 +0530

    Add these files also

---
+++ src/Sept/ex_09092024/Lap004.java
@@ -0,0 +1,9 @@
+package Sept.ex_09092024;
+
+public class Lap004 {
+    public static void main(String[] args) {
+        boolean Do_you_have_car = true;
+        boolean do_you_have_interest_in_coding = true;
+        boolean do_you_have_bmw = false;
+    }
+}
