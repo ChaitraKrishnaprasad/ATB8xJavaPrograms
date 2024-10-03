f904e03052bfe7101dcfb92ded163b2048456e4a
Chaitra Krishnaprasad <chaitra.kmsp@gmail.com>
2024-10-03 20:45:24 +0530

    Add these files also

---
+++ src/Sept/ex_16092024_Operators/Lab30_Logical_operator1.java
@@ -0,0 +1,11 @@
+package Sept.ex_16092024_Operators;
+
+public class Lab30_Logical_operator1 {
+    public static void main(String[] args) {
+        System.out.println(10 == 10);
+        System.out.println(10 >= 10);
+        System.out.println(10 <= 10);
+        System.out.println(10 > 10);
+        System.out.println(10 < 10);
+    }
+}
