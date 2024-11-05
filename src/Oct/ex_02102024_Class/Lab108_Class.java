a882d94025b2966fd6678ee5271aefd3e7c2bc3e
Chaitra Krishnaprasad <chaitra.kmsp@gmail.com>
2024-11-05 17:14:46 +0530

    Add these files also

---
+++ src/Oct/ex_02102024_Class/Lab108_Class.java
@@ -0,0 +1,27 @@
+package Oct.ex_02102024_Class;
+
+public class Lab108_Class {
+    public static void main(String[] args) {
+        // class --> Class is a template, blue print to create a object
+        //Object --> Instance of the class - created from the class
+        //Class - Lab108_Class
+        //      1) A - Attribute || data members ||Instant variable||fields ||member variables
+        //      -Common things will be attribute
+//        long phone_number;
+//        String email;
+//        int age;
+//        int batch_8x;
+//        String Name:
+//        boolean isMale;
+//        String address;
+//        boolean isfeePaid;
+//        String githublink;
+//        String S_id;
+        //      2) B - Behaviour || Methods || Member functions - specific things
+//        void study(){}
+//        void eat(){}
+//        void walk(){}
+
+
+    }
+}
