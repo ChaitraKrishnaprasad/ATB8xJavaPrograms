2aacfa3640c48e1bf30ff0613d8b2f2af047922c
Chaitra Krishnaprasad <chaitra.kmsp@gmail.com>
2024-12-30 16:41:04 +0530

    Add these files also

---
+++ src/Oct/DailyOctTasks/ex_14102024_Abstract_Interface.java
@@ -0,0 +1,55 @@
+package Oct.DailyOctTasks;
+
+public class ex_14102024_Abstract_Interface {
+    public static void main(String[] args) {
+        book myBook = new printMyBook("Harry Potter", "J.K. Rowling",100);
+        System.out.println(myBook.getDetails());
+    }
+}
+
+//Book class which has an abstract method getDetails(),name, author, price.
+//PrintMyBook class that inherits from the Book class.
+//abstract
+//Book myBook = new PrintMyBook("Harry Potter", "J.k. Rowling", "120");
+// Output
+//"Harry Potter, J.k. Rowling, 100"
+
+//Abstract class
+abstract class book{
+    private String name;
+    private String author;
+    private double prince;
+
+    //Constructor
+    public book(String name, String author, double prince) {
+        this.name = name;
+        this.author = author;
+        this.prince = prince;
+    }
+//Getter
+    public String getName() {
+        return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public double getPrince() {
+        return prince;
+    }
+    //Abstract method to get book details
+    public abstract String getDetails();
+}
+//extends class
+class printMyBook extends book{
+//constructor
+    public printMyBook(String name, String author, double prince) {
+        super(name, author, prince);
+    }
+//implementing the abstract method
+    @Override
+    public String getDetails() {
+        return getName()+", "+getAuthor()+", "+(int) getPrince();
+    }
+}
