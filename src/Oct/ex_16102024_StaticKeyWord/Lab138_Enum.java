2809715ac758a327b263c69742965bf60526c1c7
Chaitra Krishnaprasad <chaitra.kmsp@gmail.com>
2025-01-02 17:11:07 +0530

    Add these files also

---
+++ src/Oct/ex_16102024_StaticKeyWord/Lab138_Enum.java
@@ -0,0 +1,13 @@
+package Oct.ex_16102024_StaticKeyWord;
+
+public class Lab138_Enum {
+    public static void main(String[] args) {
+        //ENUM
+        System.out.println(Day.Saturday);
+
+    }
+}
+
+enum Day {
+    Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday
+}
